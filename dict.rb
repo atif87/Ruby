1a6fa0578be3d879d138bf2267e00db8a36ebfbe
Atif Iqbal <atif@localhost.localdomain>
2012-01-15 18:14:28 +0530

    3

--- dict.rb
+++ dict.rb
@@ -1,26 +1,40 @@
 #!/usr/bin/ruby
+#puts "Hello World!";
+
 %w(rubygems wordnik).each {|lib| require lib}
 
-Wordnik.configure do |config|
-    config.api_key = '26878d3b045c48a34970c008e2806530a3909c4a072354f3e'
-end
-
-# Definitions
-Wordnik.word.get_definitions('hysterical')
-Wordnik.word.get_definitions('lemurs', :use_canonical => true)
-Wordnik.word.get_definitions('fish', :part_of_speech => 'verb,noun')
-Wordnik.word.get_definitions('scoundrel', :limit => 20, :source_dictionaries => "ahd,wiktionary,wordnet")
-
-# Examples
-Wordnik.word.get_examples('slovenly')
-Wordnik.word.get_examples('wrangle', :limit => 10, :skip => 10) # pagination
-
-# Related Words
-#Wordnik.word.get_related_words('sad', :type => 'synonym')
-#Wordnik.word.get_related_words('bowls', :type => 'hypernym', :use_canonical => true)
-
-# Search
-Wordnik.words.search_words(:query => 'dog')
-Wordnik.words.search_words(:query => 'cal*', :min_dictionary_count => 3)
-Wordnik.words.search_words(:query => '*tin*', :include_part_of_speech => 'verb', :min_length => 5, :max_length => 20)
-
+	Wordnik.configure do |config|
+	    config.api_key = '26878d3b045c48a34970c008e2806530a3909c4a072354f3e'
+	    config.username = 'atif'
+	    config.password = 'damienzinta'
+	end
+	
+	
+#	Wordnik.word
+#
+	r=Wordnik.word();
+	if ARGV[0]==nil
+		w=Wordnik.words.get_word_of_the_day()
+		r.get_definitions(w);
+		r.get_examples(w);
+		r.get_related(w, :type => 'antonym');
+		r.get_related(w, :type => 'synonym');
+	end
+	if ARGV[0]=='ant'
+		x=r.get_related(ARGV[1], :type => 'antonym');
+	end
+	if ARGV[0]=='def'
+		r.get_definitions(ARGV[1]);
+	end
+	if ARGV[0]=='ex'
+		r.get_examples(ARGV[1]);
+	end	
+	if ARGV[0]=='syn'
+		r.get_related(ARGV[1], :type => 'synonym');
+	end
+	if ARGV[0]=='dict'
+		r.get_definitions(ARGV[1]);
+		r.get_examples(ARGV[1]);
+		r.get_related(ARGV[1], :type => 'antonym');
+		r.get_related(ARGV[1], :type => 'synonym');
+	end
